e095f6db4e0de1f6b8d2231ab6409fb1e23ad0ec
bookhabit <qnrgoqlt@naver.com>
2022-04-30 00:40:09 +0900

    list string tuple

--- python/list.py
+++ python/list.py
@@ -38,12 +38,82 @@
 
 
 # 리스트 컴프리헨션   (0부터 19까지의 수 중에서 홀수만 포함하는 리스트)
-array =[ i for i in range(0,20) if i%2==1]
-print(array)
+# array =[ i for i in range(0,20) if i%2==1]
+# print(array)
 
 #일반적인 코드 (0부터 19까지의 수 중에서 홀수만 포함하는 리스트)
-array = []
-for i in range(0,20) :
-    if i%2 == 1:
-        array.append(i)
-print(array)
+# array = []
+# for i in range(0,20) :
+#     if i%2 == 1:
+#         array.append(i)
+# print(array)
+
+# 리스트컴프리헨션 (좋은예시)
+# N X M 크기의 2차원 리스트 초기화 
+# n =4
+# m = 3
+# array =[[0]*m for _ in range(n)]
+# print(array)
+
+# array[1][1] = 5  # 2차원 배열의 값 수정
+# print(array)   올바른 방법은 [1][1] 1행1열에 원소가 5로 바뀌었음 
+
+# # N X M 크기의 2차원 리스트 초기화 (잘못된 방법)
+# n =4
+# m =3
+# array = [[0]*m] *n
+# print(array)
+
+# array[1][1] = 5
+# print(array)  잘못된 방법은 [1][1] 모든행에 1열 원소가 5로 바뀌었음
+
+# 언더바 사용
+# 1부터 9까지의 자연수 더하기
+# summary =0
+# for i in range(1,10):
+#     summary += i
+# print(summary)
+
+# # "Hello World" 를 5번 출력하기
+# for _ in range(5):
+#     print("Hello World")
+
+# # 리스트 관련함수들
+# a = [1,4,3]
+# print("기본 리스트 : ",a)
+
+# # 리스트에 원소 삽입
+# a.append(2)
+# print(a)
+
+# # 오름차순 정렬
+# a.sort()
+# print(a)
+
+# # 내림차순 정렬
+# a.sort(reverse=True)
+# print(a)
+
+# # 리스트 원소 뒤집기
+# a.reverse()
+# print(a)
+
+# # 특정 인덱스에 데이터 추가
+# a.insert(2,3)
+# print(a)
+
+# # 특정 값인 데이터 개수 세기
+# a.index(3)
+# print(a)
+
+# # 특정 값 데이터 삭제
+# a.remove(1)
+# print(a)
+
+# 리스트에서 특정 값을 가지는 원소를 모두 제거하기
+a = [1,2,3,4,5,5,5]
+remove_set = {3,5}  # 집합 자료형
+
+# remove_list에 포함되지 않은 값만을 저장
+result = [i for i in a  if i not in remove_set]
+print(result)
---
+++ python/string.py
@@ -0,0 +1,23 @@
+# 문자열 초기화
+# data = "Hello World"
+# print(data)
+
+# data = "Don't you know \"Python\"?"
+# print(data)
+
+# 문자열 연산
+a = "Hello"
+b = "World"
+print(a+" "+b)
+
+a = "String"
+print(a*3)
+
+# 문자열 인덱스와 슬라이싱
+a = "ABCDEF"
+print(a[2:4])
+
+# for문으로 문자열을 넣으면 문자 하나씩 반복됨
+for i in a :
+    print(i)
+
